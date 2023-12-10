bbcf1f7b2cec442da75c63847fc52d653cffa206
Joel Eaton <jeatongt@icloud.com>
2023-12-10 14:07:13 -0500

    Success!

--- Day02/possible-games-counter.py
+++ Day02/possible-games-counter.py
@@ -12,7 +12,11 @@ def is_this_game_possible(game_string, max_red, max_blue, max_green):
     blues_shown = re.findall(r'(\d+) blue', game_string)
     greens_shown = re.findall(r'(\d+) green', game_string)
     is_impossible = any(int(red) > max_red for red in reds_shown)
+    if is_impossible:
+        return 0
     is_impossible = any(int(blue) > max_blue for blue in blues_shown)
+    if is_impossible:
+        return 0
     is_impossible = any(int(green) > max_green for green in greens_shown)
     if is_impossible:
         return 0
@@ -20,7 +24,7 @@ def is_this_game_possible(game_string, max_red, max_blue, max_green):
         return int(game_number)
 
 total = 0
-with open('/Users/jeaton/Git/advent-2023/Day02/test-input.txt', 'r') as file:
+with open('/Users/jeaton/Git/advent-2023/Day02/cube-games.txt', 'r') as file:
     for line in file.readlines():
         game_string = line.strip()  # remove newline characters
         game_is_possible = is_this_game_possible(game_string, max_red, max_blue, max_green)
