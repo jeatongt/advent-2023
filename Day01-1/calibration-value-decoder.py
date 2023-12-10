b0d29ba295d8698c9605ac99fd0e086a494cf748
Joel Eaton <jeatongt@icloud.com>
2023-12-09 22:40:00 -0500

    Fixed a dum mistake

--- Day01-1/calibration-value-decoder.py
+++ Day01-1/calibration-value-decoder.py
@@ -7,7 +7,7 @@ def string_to_calibration_value(calibration_string):
     for character in calibration_string:
         if character.isnumeric():
             last_number = character
-    return int(first_number) + int(last_number)
+    return 10*int(first_number) + int(last_number)
 
 total = 0
 with open('/Users/jeaton/Git/advent-2023/Day01-1/calibration-input.txt', 'r') as file:
