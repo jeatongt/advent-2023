7afdfb1a92e48804be58cafc12c91eb089b21ef7
Joel Eaton <jeatongt@icloud.com>
2023-12-16 12:10:21 -0500

    Success!

--- Day04/count-cards-and-copies.py
+++ Day04/count-cards-and-copies.py
@@ -32,18 +32,25 @@ def score_of_this_card(card_string):
 def get_winning_copies(original_cards, cards_to_score):
     winning_copies = []
     for card in cards_to_score:
-        card_id = re.search(r'Card\s*(\d+)').group(1)
-        print(card_id)
-        for i in range(score_of_this_card(card)):
-            winning_copies.append(original_cards[int(card_id)+i+1])
+        card_id = re.search(r'Card\s*(\d+)', card).group(1)
+        card_score = score_of_this_card(card)
+        # print('Card ' + card_id + ' scored ' + str(card_score))
+        if card_score > 0:
+            for i in range(score_of_this_card(card)):
+                winning_copies.append(original_cards[int(card_id)+i].strip())
     return winning_copies
 
 
 def get_count_of_these_cards_and_their_copies(original_cards, cards_to_score):
-    card_count = get_count_of_these_cards_and_their_copies(original_cards, get_winning_copies(original_cards, cards_to_score))+len(cards_to_score)
-    return card_count
+    winning_copies = get_winning_copies(original_cards, cards_to_score)
+    if winning_copies:
+        # print(str(len(winning_copies)) + ' winning copies')
+        # print(winning_copies)
+        return get_count_of_these_cards_and_their_copies(original_cards, winning_copies) + len(cards_to_score)
+    else:
+        return len(cards_to_score)
 
-with open('/Users/jeaton/Git/advent-2023/Day04/test-input.txt', 'r') as file:
+with open('/Users/jeaton/Git/advent-2023/Day04/scratchcards.txt', 'r') as file:
     original_cards = file.readlines()
     total_count = get_count_of_these_cards_and_their_copies(original_cards, original_cards)
     print(total_count)
