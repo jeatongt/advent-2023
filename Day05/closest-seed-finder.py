f5d9a71e0278ec24732184580048f088b2e9e3ec
Joel Eaton <jeatongt@icloud.com>
2023-12-16 21:15:27 -0500

    map maker

--- Day05/closest-seed-finder.py
+++ Day05/closest-seed-finder.py
@@ -1,6 +1,15 @@
 # python
 import re
 
+def make_map(map_lines):
+    map = {}
+    for line in map_lines:
+        matches = re.findall(r'(\d+)\s*(\d+)\s*(\d+)', line)
+        if matches:
+            for i in range(matches[2]):
+                map[int(matches[1]) + i] = int(matches[0]) + i
+    return map
+
 with open('/Users/jeaton/Git/advent-2023/Day05/test-map.txt', 'r') as file:
     map_file_full = file.readlines()
     map_file = [line.strip() for line in map_file_full]
@@ -13,3 +22,24 @@ with open('/Users/jeaton/Git/advent-2023/Day05/test-map.txt', 'r') as file:
                 for match in matches:
                     seeds = [int(num) for num in re.findall(r'\d+', match)]
                     print(seeds)
+        if re.search(r'map:', map_file[i]):
+            map_name = re.search(r'(.*)\smap:)', map_file[i]).group(1)
+            map_lines = []
+            while map_file[i+1] != '':
+                map_lines.append(map_file[i+1])
+                i += 1
+            if map_name == 'seed-to-soil':
+                seed_soil_map = make_map(map_lines)
+                print(seed_soil_map)
+            if map_name == 'soil-to-fertilizer':
+                soil_fertilizer_map = make_map(map_lines)
+            if map_name == 'fertilizer-to-water':
+                fertilizer_water_map = make_map(map_lines)
+            if map_name == 'water-to-light':
+                water_light_map = make_map(map_lines)
+            if map_name == 'light-to-temperature':
+                light_temperature_map = make_map(map_lines)
+            if map_name == 'temperature-to-humidity':
+                temperature_humidity_map = make_map(map_lines)
+            if map_name == 'humidity-to-location':
+                humidity_location_map = make_map(map_lines)
