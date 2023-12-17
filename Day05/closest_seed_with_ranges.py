dd1919e796dd96eb7195b84fd3ef19513f21827e
Joel Eaton <jeatongt@icloud.com>
2023-12-17 18:08:29 -0500

    Doing it all with ranges

--- Day05/closest_seed_with_ranges.py
+++ Day05/closest_seed_with_ranges.py
@@ -6,25 +6,34 @@ import operator
 # seed-to-soil map:
 # 50 98 2
 # 52 50 48
-# This function needs to accept a range instead of a key, and return a list of ranges
-def get_map_value(map_lines, key):
-    for line in map_lines:
-        matches = re.findall(r'(\d+)\s*(\d+)\s*(\d+)', line)
-        if matches:
-            for match in matches:
-                if int(match[1]) <= int(key) <= int(match[1]) + int(match[2]) - 1:
-                    return int(match[0]) + int(key) - int(match[1])
-    return key
-
-def get_best_location(start, seed_range):
-    best_seed = None
-    best_location = None
-    for i in range(int(seed_range)):
-        location = get_map_value(humidity_location_map, get_map_value(temperature_humidity_map, get_map_value(light_temperature_map, get_map_value(water_light_map, get_map_value(fertilizer_water_map, get_map_value(soil_fertilizer_map, get_map_value(seed_soil_map, int(start) + i)))))))
-        if best_location is None or location < best_location:
-            best_seed = int(start) + i
-            best_location = location
-    return best_seed, best_location
+# get_map_ranges(["50 98 2", "52 50 48"], {'79': '20', '55': '13'}) 
+#     -> {'81': '19', '57': '13', '50': '1'}
+#     -> {'79+52-50': '20-(79+20-50-48)', '57': '13', get_map_ranges(["50 98 2", "52 50 48"], {'98': '1'})
+# This function needs to accept a dict of ranges instead of a key, and return a dict of ranges
+def get_map_ranges(map_lines, source_dict):
+    destination_dict = {}
+    additional_ranges_dict = {}
+    for key, value in source_dict.items():
+        dest_temp_dict = {}
+        for line in map_lines:
+            matches = re.findall(r'(\d+)\s*(\d+)\s*(\d+)', line)
+            if matches:
+                for match in matches:
+                    if int(match[1]) <= key <= int(match[1]) + int(match[2]) - 1:
+                        if value <= int(match[2]):
+                            dest_temp_dict[key + int(match[0]) - int(match[1])] = value
+                        else:
+                            start_diff = int(match[0]) - int(match[1])
+                            range_diff = key + value - int(match[1]) - int(match[2])
+                            dest_temp_dict[key + start_diff] = value - range_diff
+                            additional_ranges_dict[key + value - range_diff] = range_diff
+        if dest_temp_dict:
+            destination_dict.update(dest_temp_dict)
+        else:
+            destination_dict[key] = value
+    if additional_ranges_dict:
+        destination_dict.update(get_map_ranges(map_lines, additional_ranges_dict))
+    return destination_dict
 
 with open('/Users/jeaton/Git/advent-2023/Day05/test-map.txt', 'r') as file:
     map_file_full = file.readlines()
@@ -36,7 +45,7 @@ with open('/Users/jeaton/Git/advent-2023/Day05/test-map.txt', 'r') as file:
             matches = re.findall(r'(\d+)\s*(\d+)', map_file[i])
             if matches:
                 for match in matches:
-                    seeds[match[0]] = match[1]
+                    seeds[int(match[0])] = int(match[1])
         if re.search(r'map:', map_file[i]):
             map_name = re.search(r'(.*)\smap:', map_file[i]).group(1)
             map_lines = []
@@ -57,12 +66,19 @@ with open('/Users/jeaton/Git/advent-2023/Day05/test-map.txt', 'r') as file:
                 temperature_humidity_map = map_lines
             if map_name == 'humidity-to-location':
                 humidity_location_map = map_lines
-seed_location_map = {}
 print(seeds)
-for key, value in seeds.items():
-    location_map_seed, location_map_location = get_best_location(key, value)
-    print("Best location for seed range " + key + " is " + str(location_map_seed) + " at " + str(location_map_location))
-    seed_location_map[location_map_seed] = location_map_location
-sorted_seed_location_map = dict(sorted(seed_location_map.items(), key=operator.itemgetter(1)))
-first_key, first_value = next(iter(sorted_seed_location_map.items()))
-print(f"Seed: {first_key}, Location: {first_value}")
+soil_map = get_map_ranges(seed_soil_map, seeds)
+print(soil_map)
+fertilizer_map = get_map_ranges(soil_fertilizer_map, soil_map)
+print(fertilizer_map)
+water_map = get_map_ranges(fertilizer_water_map, fertilizer_map)
+print(water_map)
+light_map = get_map_ranges(water_light_map, water_map)
+print(light_map)
+temperature_map = get_map_ranges(light_temperature_map, light_map)
+print(temperature_map)
+humidity_map = get_map_ranges(temperature_humidity_map, temperature_map)
+print(humidity_map)
+location_map = get_map_ranges(humidity_location_map, humidity_map)
+print(location_map)
+print('Best location is ' + str(min(location_map)))
