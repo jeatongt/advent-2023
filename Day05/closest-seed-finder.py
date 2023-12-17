5a11859e2f6c7c9dd2f6bd12835ba754f9454374
Joel Eaton <jeatongt@icloud.com>
2023-12-16 22:04:58 -0500

    Success!

--- Day05/closest-seed-finder.py
+++ Day05/closest-seed-finder.py
@@ -2,21 +2,14 @@
 import re
 import operator
 
-def make_map(map_lines):
-    map = {}
+def get_map_value(map_lines, key):
     for line in map_lines:
         matches = re.findall(r'(\d+)\s*(\d+)\s*(\d+)', line)
         if matches:
             for match in matches:
-                for i in range(int(match[2])):
-                    map[int(match[1]) + i] = int(match[0]) + i
-    return map
-
-def get_map_value(map, key):
-    if key in map:
-        return map[key]
-    else:
-        return key
+                if int(match[1]) <= key <= int(match[1]) + int(match[2]) - 1:
+                    return int(match[0]) + key - int(match[1])
+    return key
 
 with open('/Users/jeaton/Git/advent-2023/Day05/full-map.txt', 'r') as file:
     map_file_full = file.readlines()
@@ -37,20 +30,19 @@ with open('/Users/jeaton/Git/advent-2023/Day05/full-map.txt', 'r') as file:
                 map_lines.append(map_file[i+1])
                 i += 1
             if map_name == 'seed-to-soil':
-                seed_soil_map = make_map(map_lines)
-                # print(seed_soil_map)
+                seed_soil_map = map_lines
             if map_name == 'soil-to-fertilizer':
-                soil_fertilizer_map = make_map(map_lines)
+                soil_fertilizer_map = map_lines
             if map_name == 'fertilizer-to-water':
-                fertilizer_water_map = make_map(map_lines)
+                fertilizer_water_map = map_lines
             if map_name == 'water-to-light':
-                water_light_map = make_map(map_lines)
+                water_light_map = map_lines
             if map_name == 'light-to-temperature':
-                light_temperature_map = make_map(map_lines)
+                light_temperature_map = map_lines
             if map_name == 'temperature-to-humidity':
-                temperature_humidity_map = make_map(map_lines)
+                temperature_humidity_map = map_lines
             if map_name == 'humidity-to-location':
-                humidity_location_map = make_map(map_lines)
+                humidity_location_map = map_lines
 seed_location_map = {}
 for seed in seeds:
     seed_location_map[seed] = get_map_value(humidity_location_map, get_map_value(temperature_humidity_map, get_map_value(light_temperature_map, get_map_value(water_light_map, get_map_value(fertilizer_water_map, get_map_value(soil_fertilizer_map, get_map_value(seed_soil_map, seed)))))))
