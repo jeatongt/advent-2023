cb2e681fc382897eff7dc21a7b3b2d85780fbe78
Joel Eaton <jeatongt@icloud.com>
2023-12-18 21:46:54 -0500

    Scoring functions done

--- Day07/score-hands.py
+++ Day07/score-hands.py
@@ -23,8 +23,49 @@ def score_of_this_hand(hand):
 
 def five_of_a_kind(hand):
     same_card = Counter(hand)
+    for rank in same_card:
+        if same_card[rank] == 5:
+            return True
+    return False
 
-    
+def four_of_a_kind(hand):
+    same_card = Counter(hand)
+    for rank in same_card:
+        if same_card[rank] == 4:
+            return True
+    return False
+
+def full_house(hand):
+    same_card = Counter(hand)
+    if len(same_card) == 2:
+        return True
+    return False
+
+def three_of_a_kind(hand):
+    same_card = Counter(hand)
+    for rank in same_card:
+        if same_card[rank] == 3:
+            return True
+    return False
+
+def two_pair(hand):
+    same_card = Counter(hand)
+    if len(same_card) == 3:
+        return True
+    return False
+
+def one_pair(hand):
+    same_card = Counter(hand)
+    if len(same_card) == 4:
+        return True
+    return False
+
+def high_cards(hand):
+    card_values = ''
+    rank_values = {'A': 'a', 'K': 'b', 'Q': 'c', 'J': 'd', 'T': 'e', '9': 'f', '8': 'g', '7': 'h', '6': 'i', '5': 'j', '4': 'k', '3': 'l', '2': 'm'}
+    for card in hand:
+        card_values += rank_values[card]
+    return card_values    
 
 hands = []
 with open('/Users/jeaton/Git/advent-2023/Day07/test-hands.txt', 'r') as file:
@@ -34,6 +75,12 @@ with open('/Users/jeaton/Git/advent-2023/Day07/test-hands.txt', 'r') as file:
         if cards:
             this_hand = list(cards[0])
             this_hand.append(cards[1])
+            this_hand.append(score_of_this_hand(this_hand[:5]))
         hands.append(this_hand)
 print(hands)
+sorted_hands = sorted(hands, key=lambda x: x[6], reverse=True)
+winnings = 0
+for i in len(sorted_hands):
+    winnings += int(sorted_hands[i][5])*(i+1)
+print(winnings)
     
