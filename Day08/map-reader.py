f030c7ce27b85ef3631a0e5cdc2c32055c033aba
Joel Eaton <jeatongt@icloud.com>
2023-12-19 18:55:54 -0500

    Success!

--- Day08/map-reader.py
+++ Day08/map-reader.py
@@ -2,17 +2,33 @@
 import re
 
 map_lines = []
-with open('/Users/jeaton/Git/advent-2023/Day08/test-map.txt', 'r') as file:
+with open('/Users/jeaton/Git/advent-2023/Day08/full-map.txt', 'r') as file:
     for line in file.readlines():
         map_lines.append(line.strip())
-    print(map_lines)
     turns = list(map_lines[0])
     map_nodes = {}
     for i in range(2, len(map_lines)):
-        matches = re.findall(r'[A-Z]+', map_lines[i])
+        matches = re.findall(r'([A-Z]+)', map_lines[i])
         if matches:
-            for match in matches:
-                map_nodes[match[0]] = [match[1], match[2]]
+            map_nodes[matches[0]] = [matches[1], matches[2]]
 print(turns)
 print(map_nodes)
-        
+
+current_node = 'AAA'
+steps_taken = 0
+print(current_node)
+while current_node != 'ZZZ':
+    for turn in turns:
+        if turn == 'L':
+            current_node = map_nodes[current_node][0]
+            print(current_node)
+            steps_taken += 1
+            if current_node == 'ZZZ':
+                break
+        if turn == 'R':
+            current_node = map_nodes[current_node][1]
+            print(current_node)
+            steps_taken += 1
+            if current_node == 'ZZZ':
+                break
+print(steps_taken)
